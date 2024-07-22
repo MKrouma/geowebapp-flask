05c319c5d7cf03ef287989adbe815d068e86fa1f
devvamoussa <84201629+devvamoussa@users.noreply.github.com>
2024-07-22 23:16:53 +0000

    dealing with render - default port 10000

--- app/app.py
+++ app/app.py
@@ -207,5 +207,5 @@ def get_closest_markets(params) :
     return closest_markets
 
 if __name__ == "__main__" : 
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
